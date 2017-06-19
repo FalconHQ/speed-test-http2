7160168649b500a92572fe4688c9ffa0aa178e90
Poyu Chen <p820316@gmail.com>
2017-06-19 12:24:37 -0700

    Refactored code, better file structure

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
 const express = require('express');
-const spdy = require('spdy')
-const fs = require('mz/fs')
+const h2 = require('spdy')
+// const h2 = require('http2')
+// const h2 = require('https')
+const fs = require('fs')
+const path = require('path')
 const debug = require('debug') ('server')
 const spParser = require('./themiddleware.js')
 
+// express.request.__proto__ = h2.IncomingMessage.prototype;
+// express.response.__proto__ = h2.ServerResponse.prototype;
+
 const app = express();
 
-app.use(express.static('.'))
+// const jq = fs.readFileSync('./jquery.min.js')
+// const processing = fs.readFileSync('./processing.min.js')
+const index = fs.readFileSync('./index.html')
 
-// app.get('/', spParser,  (req, res) =>{
-//     res.sp('index.html')
+// app.use(express.static('.'))
+
+// app.get('/', (req, res) => {
+//   let push = res.push('/jquery.min.js')
+//   push.writeHead(200)
+//   fs.createReadStream(path.join(__dirname, '/jquery.min.js')).pipe(push);
+//   push = res.push('/processing.min.js')
+//   push.writeHead(200)
+//   fs.createReadStream(path.join(__dirname, '/processing.min.js')).pipe(push);
+//   res.end(index);
 // })
+
+app.get('/', spParser,  (req, res) =>{
+    res.sp('index.html')
+})
 
 const PORT = 3000;
 
-spdy.createServer({
+// app.listen(3000)
+
+h2.createServer({
     key: fs.readFileSync('./key.pem'),
     cert: fs.readFileSync('./cert.pem')
 }, app)
--- themiddleware.js
+++ themiddleware.js
@@ -1,6 +1,7 @@
 const debug = require('debug')('spParser')
 const fs = require('fs')
 const extractor = require('html-static-asset-path-extractor')
+const path = require('path')
 //USE POYU MODULE
 
 const readFileAsync = (filepath) => {
@@ -19,14 +20,21 @@ const pushSingle = (res, resource) => {
     return new Promise((resolve, reject) => {
         const {filePath, contentType} = resource
         if (!res.push) reject(new Error('NO SPDY'))
-        readFileAsync(filePath)
-        .then(file => {
-            let pushStream = res.push('/' + filePath, {
-                req: { 'accept' : '**/*'},
-                res: {'content-type' : contentType}
-            }, (file) => resolve({ value: file, status: "resolved"}))
-            pushStream.end(file)
-        }, (error) => resolve({value:error, status: "rejected"}))
+
+        let pushStream = res.push('/' + filePath, {
+            req: { 'accept' : '**/*'},
+            res: {'content-type' : contentType}
+        }, (file) => resolve({ value: file, status: "resolved"}))
+        fs.createReadStream(path.join(__dirname, filePath)).pipe(pushStream);
+
+        // readFileAsync(filePath)
+        // .then(file => {
+        //     let pushStream = res.push('/' + filePath, {
+        //         req: { 'accept' : '**/*'},
+        //         res: {'content-type' : contentType}
+        //     }, (file) => resolve({ value: file, status: "resolved"}))
+        //     pushStream.end(file)
+        // }, (error) => resolve({value:error, status: "rejected"}))
         // .catch(err => {
         //     debug('err', err)
         //     return Promise.reject(res);
@@ -37,7 +45,7 @@ const pushSingle = (res, resource) => {
 const spParser = (req, res, next) => {
     const sp = (htmlPath) => {
         const resources = extractor(htmlPath)
-        debug('adsfasdf', resources)
+        // debug('adsfasdf', resources)
         // const resources =  [ {filePath: 'awesum.jpg', contentType: 'img/jpeg'}, {filePath: 'salt.jpg', contentType: 'img/jpeg'} ]
         const PromiseArr = resources.map(cur => pushSingle(res, cur))
 
@@ -45,8 +53,7 @@ const spParser = (req, res, next) => {
         .then((files)=>{
             const html = readFileAsync(htmlPath).then((file) => {
                 res.status(200);
-                res.end(html)
-                debug('in promiseall!!!!', html)
+                res.end(file)
             })
         })
         .catch((err)=>{
